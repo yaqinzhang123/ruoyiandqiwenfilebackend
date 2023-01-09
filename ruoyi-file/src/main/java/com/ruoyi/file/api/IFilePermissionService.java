49815b9e5752306e754d4f35376cb9a311a26e63
yaqinzhang123 <822763756@qq.com>
2023-01-09 08:26:11 +0800

    删除ruoyi-file，先整合若依为mybatisplus

--- ruoyi-file/src/main/java/com/ruoyi/file/api/IFilePermissionService.java
+++
@@ -1,8 +0,0 @@
-package com.ruoyi.file.api;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.ruoyi.file.domain.FilePermission;
-
-public interface IFilePermissionService extends IService<FilePermission> {
-
-}
--- ruoyi-file/src/main/java/com/ruoyi/file/mapper/PictureFileMapper.java
+++
@@ -1,7 +0,0 @@
-package com.ruoyi.file.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.ruoyi.file.domain.PictureFile;
-
-public interface PictureFileMapper extends BaseMapper<PictureFile> {
-}
